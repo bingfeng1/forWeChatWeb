4a7bb7ad82a496ce196677d1816283efc8cb154b
冰风 <wuxiaoming@baosight.com>
2019-07-25 10:49:14 +0800

    搭载scss环境，增加elementui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
